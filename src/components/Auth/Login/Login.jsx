181706e478309ec4f1edd7fe4c3f65948431e70b
successfulcode <sergej.mickevic@gmail.com>
2021-06-14 21:59:00 +0300

    Made test account for demo

--- src/components/Auth/Login/Login.jsx
+++ src/components/Auth/Login/Login.jsx
@@ -8,167 +8,178 @@ import { NavLink, Redirect } from 'react-router-dom';
 import HrWithCentedText from '../HrWithCentedText/HrWithCentedText';
 
 class Login extends React.Component {
+  state = {
+    isFormValid: false,
+    formControls: {
+      email: {
+        value: 'test@test.lt',
+        type: 'email',
+        autocomplete: 'username',
+        label: 'El. paštas',
+        errorMessage: 'Įveskite teisingą el.paštą!',
+        valid: false,
+        touched: false,
+        validation: {
+          required: true,
+          email: true,
+        },
+      },
+      password: {
+        value: '123456',
+        type: 'password',
+        autocomplete: 'current - password',
+        label: 'Slaptažodis',
+        errorMessage: 'Slaptažodis ne mažiau 6 simbolių!',
+        valid: false,
+        touched: false,
+        validation: {
+          required: true,
+          minLength: 6,
+        },
+      },
+    },
+  };
 
-    state = {
-        isFormValid: false,
-        formControls: {
-            email: {
-                value: '',
-                type: 'email',
-                autocomplete: 'username',
-                label: 'El. paštas',
-                errorMessage: 'Įveskite teisingą el.paštą!',
-                valid: false,
-                touched: false,
-                validation: {
-                    required: true,
-                    email: true
-                }
-            },
-            password: {
-                value: '',
-                type: 'password',
-                autocomplete: 'current - password',
-                label: 'Slaptažodis',
-                errorMessage: 'Slaptažodis ne mažiau 6 simbolių!',
-                valid: false,
-                touched: false,
-                validation: {
-                    required: true,
-                    minLength: 6
-                }
-            }
-        }
-    };
+  loginHandler = () => {
+    this.props.auth(
+      this.state.formControls.email.value,
+      this.state.formControls.password.value,
+      true
+    );
+  };
 
-    loginHandler = () => {
-        this.props.auth(
-            this.state.formControls.email.value,
-            this.state.formControls.password.value,
-            true
-        )
-    };
+  registernHandler = () => {
+    this.props.auth(
+      this.state.formControls.email.value,
+      this.state.formControls.password.value,
+      false
+    );
+  };
 
-    registernHandler = () => {
-        this.props.auth(
-            this.state.formControls.email.value,
-            this.state.formControls.password.value,
-            false
-        )
-    };
+  submitHandler = (event) => {
+    event.preventDefault();
+  };
 
-    submitHandler = (event) => { event.preventDefault() };
+  validateControl(value, validation) {
+    if (!validation) {
+      return true;
+    }
+    let isValid = true;
+    if (validation.required) {
+      isValid = value.trim() !== '' && isValid;
+    }
+    if (validation.email) {
+      isValid = is.email(value) && isValid;
+    }
+    if (validation.minLength) {
+      isValid = value.length >= validation.minLength && isValid;
+    }
+    return isValid;
+  }
 
-    validateControl(value, validation) {
-        if (!validation) { return true }
-        let isValid = true
-        if (validation.required) {
-            isValid = value.trim() !== '' && isValid
-        }
-        if (validation.email) {
-            isValid = is.email(value) && isValid
-        }
-        if (validation.minLength) {
-            isValid = value.length >= validation.minLength && isValid
-        }
-        return isValid
-    };
+  onChangeHandler = (event, controlName) => {
+    const formControls = { ...this.state.formControls };
+    const control = { ...formControls[controlName] };
 
-    onChangeHandler = (event, controlName) => {
-        const formControls = { ...this.state.formControls }
-        const control = { ...formControls[controlName] }
+    control.value = event.target.value;
+    control.touched = true;
+    control.valid = this.validateControl(control.value, control.validation);
+    formControls[controlName] = control;
 
-        control.value = event.target.value
-        control.touched = true
-        control.valid = this.validateControl(control.value, control.validation)
-        formControls[controlName] = control
+    let isFormValid = true;
 
-        let isFormValid = true
+    Object.keys(formControls).forEach((name) => {
+      isFormValid = formControls[name].valid && isFormValid;
+    });
 
-        Object.keys(formControls).forEach(name => {
-            isFormValid = formControls[name].valid && isFormValid
-        })
+    this.setState({ formControls, isFormValid });
+  };
+  renderInputs() {
+    return Object.keys(this.state.formControls).map((controlName, index) => {
+      const control = this.state.formControls[controlName];
+      return (
+        <Input
+          key={controlName + index}
+          type={control.type}
+          autocomplete={control.autocomplete}
+          value={control.value}
+          valid={control.valid}
+          touched={control.touched}
+          label={control.label}
+          shouldValidate={!!control.validation}
+          errorMessage={control.errorMessage}
+          onChange={(event) => this.onChangeHandler(event, controlName)}
+          autoComplete={control.autocomplete}
+        />
+      );
+    });
+  }
 
-        this.setState({ formControls, isFormValid })
-    };
-    renderInputs() {
-        return Object.keys(this.state.formControls).map((controlName, index) => {
-            const control = this.state.formControls[controlName]
-            return (
-                <Input
-                    key={controlName + index}
-                    type={control.type}
-                    autocomplete={control.autocomplete}
-                    value={control.value}
-                    valid={control.valid}
-                    touched={control.touched}
-                    label={control.label}
-                    shouldValidate={!!control.validation}
-                    errorMessage={control.errorMessage}
-                    onChange={event => this.onChangeHandler(event, controlName)}
-                    autoComplete={control.autocomplete}
-                />
-            );
-        });
-    };
+  render() {
+    const { authIsLoading, authError, tokenTrue, resetAuthError } = this.props;
 
-    render() {
-        const { authIsLoading, authError, tokenTrue, resetAuthError } = this.props
+    if (tokenTrue) {
+      return <Redirect to='/' />;
+    }
+    if (authError) {
+      setTimeout(() => {
+        resetAuthError();
+      }, 3000);
+    }
 
-        if (tokenTrue) { return <Redirect to='/' /> }
-        if (authError) { setTimeout(() => { resetAuthError() }, 3000); }
+    const componentLoader = (
+      <div className='spinner-border spinner-border-sm' role='status'>
+        <span className='sr-only' />
+      </div>
+    );
 
-        const componentLoader =
-            <div className='spinner-border spinner-border-sm' role='status'>
-                <span className="sr-only" />
-            </div>;
+    return (
+      <div>
+        <div className={classes.Login}>
+          <div
+            className={`${'jumbotron'}`}
+            style={{ backgroundColor: 'rgb(255,255,255, 0.5)' }}>
+            <h1>TODO</h1>
+            <form onSubmit={this.submitHandler}>
+              {this.renderInputs()}
 
-        return (
-            <div >
-                <div className={classes.Login} >
-                    <div className={`${'jumbotron'}`} style={{ backgroundColor: 'rgb(255,255,255, 0.5)' }}>
-                        <h1>
-                            TODO
-                        </h1>
-                        <form onSubmit={this.submitHandler} >
-                            {this.renderInputs()}
-                            
-                            {authError &&
-                                <div className='alert alert-dismissible alert-danger'>
-                                    {authError}
-                                </div>
-                            }
-
-                            <button type='success' className='btn btn-success' onClick={this.loginHandler}
-                                disabled={!this.state.isFormValid || authIsLoading}
-                                style={{ marginTop: '1.3rem', marginBottom: '.1rem' }}
-                            >
-                                {
-                                    authIsLoading ?
-                                        componentLoader
-                                        :
-                                        'Prisijungti'
-                                }
-                            </button>
-                            <HrWithCentedText hrText={'ARBA'} />
-                            <NavLink to='/Registration' >
-                                <button type='primary' className='btn btn-primary' onClick={this.signUp}
-                                >Registruotis</button>
-                            </NavLink>
-                        </form>
-                    </div>
+              {authError && (
+                <div className='alert alert-dismissible alert-danger'>
+                  {authError}
                 </div>
-            </div>
-        )
-    };
-};
+              )}
+
+              <button
+                type='success'
+                className='btn btn-success'
+                onClick={this.loginHandler}
+                disabled={!this.state.isFormValid || authIsLoading}
+                style={{ marginTop: '1.3rem', marginBottom: '.1rem' }}>
+                {authIsLoading ? componentLoader : 'Prisijungti'}
+              </button>
+              <HrWithCentedText hrText={'ARBA'} />
+              <NavLink to='/Registration'>
+                <button
+                  type='primary'
+                  className='btn btn-primary'
+                  onClick={this.signUp}>
+                  Registruotis
+                </button>
+              </NavLink>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
 const mapStateToProps = (state) => {
-    return {
-        authIsLoading: state.auth.authIsLoading,
-        tokenTrue: !!state.auth.token,
-        authError: state.auth.authError
-    };
+  return {
+    authIsLoading: state.auth.authIsLoading,
+    tokenTrue: !!state.auth.token,
+    authError: state.auth.authError,
+  };
 };
 
 export default connect(mapStateToProps, { auth, resetAuthError })(Login);
--- src/redux/actions/actionsTypes.js
+++ src/redux/actions/actionsTypes.js
@@ -1,23 +1,23 @@
-export const ADD_NEW_ITEM = 'redux/tasksReducer/ADD_NEW_ITEM'
-export const DELTE_ITEM = 'redux/tasksReducer/DELTE_ITEM'
-export const ON_TOGGLE_IMPORTANT = 'redux/tasksReducer/ON_TOGGLE_IMPORTANT'
-export const ON_TOGGLE_DONE = 'redux/tasksReducer/ON_TOGGLE_DONE'
-export const ON_SEARCH_CHANGE = 'redux/tasksReducer/ON_SEARCH_CHANGE'
-export const ON_FILTER_CHANGE = 'redux/tasksReducer/ON_FILTER_CHANGE'
-export const IS_LOADING = 'redux/tasksReducer/IS_LOADING'
-export const SET_TASKS = 'redux/tasksReducer/SET_TASKS'
-export const WRITE_TASK_IN_PROGRESS = 'redux/tasksReducer/WRITE_TASK_IN_PROGRESS'
-export const DELETE_LOADING = 'redux/tasksReducer/DELETE_LOADING'
-export const IMPORTANT_LOADING = 'redux/tasksReducer/IMPORTANT_LOADING'
+export const ADD_NEW_ITEM = 'redux/tasksReducer/ADD_NEW_ITEM';
+export const DELTE_ITEM = 'redux/tasksReducer/DELTE_ITEM';
+export const ON_TOGGLE_IMPORTANT = 'redux/tasksReducer/ON_TOGGLE_IMPORTANT';
+export const ON_TOGGLE_DONE = 'redux/tasksReducer/ON_TOGGLE_DONE';
+export const ON_SEARCH_CHANGE = 'redux/tasksReducer/ON_SEARCH_CHANGE';
+export const ON_FILTER_CHANGE = 'redux/tasksReducer/ON_FILTER_CHANGE';
+export const IS_LOADING = 'redux/tasksReducer/IS_LOADING';
+export const SET_TASKS = 'redux/tasksReducer/SET_TASKS';
+export const WRITE_TASK_IN_PROGRESS =
+  'redux/tasksReducer/WRITE_TASK_IN_PROGRESS';
+export const DELETE_LOADING = 'redux/tasksReducer/DELETE_LOADING';
+export const IMPORTANT_LOADING = 'redux/tasksReducer/IMPORTANT_LOADING';
 
-export const RESET_STATE = 'redux/tasksReducer/IMPORTANT_LOADING_RESET_STATE'
+export const RESET_STATE = 'redux/tasksReducer/IMPORTANT_LOADING_RESET_STATE';
 
-export const WRITE_ERROR = 'redux/tasksReducer/WRITE_ERROR'
-export const RESET_TASK_ERROR = 'redux/tasksReducer/RESET_TASK_ERROR'
+export const WRITE_ERROR = 'redux/tasksReducer/WRITE_ERROR';
+export const RESET_TASK_ERROR = 'redux/tasksReducer/RESET_TASK_ERROR';
 
-export const AUTH_SUCCESS = 'redux/authReducer/AUTH_SUCCESS'
-export const AUTH_LOGOUT = 'redux/authReducer/AUTH_LOGOUT'
-export const TOGGLE_AUTH_LOADING = 'redux/authReducer/TOGGLE_AUTH_LOADING'
-export const WRITE_AUTH_ERROR = 'redux/authReducer/WRITE_AUTH_ERROR'
-export const RESET_AUTH_ERROR = 'redux/authReducer/RESET_AUTH_ERROR'
-
+export const AUTH_SUCCESS = 'redux/authReducer/AUTH_SUCCESS';
+export const AUTH_LOGOUT = 'redux/authReducer/AUTH_LOGOUT';
+export const TOGGLE_AUTH_LOADING = 'redux/authReducer/TOGGLE_AUTH_LOADING';
+export const WRITE_AUTH_ERROR = 'redux/authReducer/WRITE_AUTH_ERROR';
+export const RESET_AUTH_ERROR = 'redux/authReducer/RESET_AUTH_ERROR';
